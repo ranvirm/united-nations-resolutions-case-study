584b53ca77e58f3524aa09c957ff17094fada1c8
ranvirm <mohanlalranvir@gmail.com>
2022-05-04 09:51:34 +0200

    + magic

--- dashboard/main_page.py
+++ dashboard/main_page.py
@@ -38,6 +38,7 @@ st.title("Case Study: United Nations Resolutions", )
 
 # load data
 data_dir = "data/feature_data"
+# data_dir = "../data/feature_data"
 conflicts = pd.read_csv(f"{data_dir}/conflicts")
 resolutions = pd.read_csv(f"{data_dir}/resolutions")
 members = pd.read_csv(f"{data_dir}/members")
@@ -64,6 +65,8 @@ un_sessions = un_sessions[un_sessions['year'] >= start_year][un_sessions['year']
 
 st.header("UN Resolutions")
 
+st.subheader("Resolutions Passed Over Time")
+
 fig1 = make_subplots(specs=[[{"secondary_y": True}]])
 
 # Add traces
@@ -76,8 +79,6 @@ fig1.add_trace(
     go.Scatter(x=un_sessions['year'], y=un_sessions["percent_passed"], name="% Resolutions Passed"),
     secondary_y=True,
 )
-
-# st.plotly_chart(fig1, use_container_width=True)
 
 # members vs passed
 fig2 = make_subplots(specs=[[{"secondary_y": True}]])
@@ -93,7 +94,6 @@ fig2.add_trace(
     secondary_y=True,
 )
 
-# st.plotly_chart(fig2, use_container_width=True)
 
 r1Col1, r1Col2 = st.columns(2)
 
@@ -103,12 +103,29 @@ with r1Col1:
 with r1Col2:
     st.plotly_chart(fig2, use_container_width=True)
 
+
+st.text('While there appears to be no relation between the volume of resolutions tabled and those that are passed,')
+st.text('the percentage of resolutions passed seems to be positively correlated with the total number of voting members.')
+
+st.subheader("How Close Were the Votes?")
+st.text('Understanding the closeness of votes could give us insight into how unanimous votes were.')
+st.text('Resolutions with low vote margins were wholly agreed upon, whether passed or not and vice versa.')
+st.text('Below we explore how close votes were using a calculated vote margin which is the difference between number of YES and NO  votes.')
+st.text('A higher vote margin indicates a more unanimous decision while those with low margins indicate a more indecisive vote.')
+
+
+
 vote_margin_all = px.histogram(
     resolutions,
     x='vote_margin',
 )
 
 st.plotly_chart(vote_margin_all, use_container_width=True)
+
+st.caption("Histogram of all vote margins")
+st.text('The above indicates that the majority of votes were unanimous.')
+st.text('Does this view change when looking at resolutions passed and not passed separately?')
+
 
 vote_margin_passed = px.histogram(
     resolutions[resolutions['resolution_passed'] == 1],
@@ -117,6 +134,9 @@ vote_margin_passed = px.histogram(
 
 st.plotly_chart(vote_margin_passed, use_container_width=True)
 
+st.caption("Histogram of all vote margins for resolutions that were passed")
+st.text('The above indicates that the majority of votes where the resolution was passed were unanimous.')
+
 vote_margin_not_passed = px.histogram(
     resolutions[resolutions['resolution_passed'] == 0],
     x='vote_margin',
@@ -124,42 +144,104 @@ vote_margin_not_passed = px.histogram(
 
 st.plotly_chart(vote_margin_not_passed, use_container_width=True)
 
-# n members by year joined
-members_year_joined_df = pysqldf(q="select count(country) as n_members, year_joined as year from members group by year_joined")
-members_year_joined = make_subplots(specs=[[{"secondary_y": False}]])
+st.caption("Histogram of all vote margins for resolutions that were not passed")
+st.text('The above shows that the majority of votes not passed were not passed with a high margin.')
+st.text('This indicates that resolutions which were not passed, were not passed with very close votes.')
+st.text('This could represent opportunities for swinging the vote by convincing just a few members to change their votes on these resolutions.')
+st.text('More analysis needs to be done on the topics which had a low vote margin to understand how to influence future votes on those topics.')
+
+# vote margin by number of members
+vote_margin_n_members_df = pysqldf(q="select avg(vote_margin) as av_vote_margin, avg(n_members) as n_members, year from resolutions group by year")
+vote_margin_n_members = make_subplots(specs=[[{"secondary_y": False}]])
 
 # Add traces
-members_year_joined.add_trace(
-    go.Scatter(x=un_sessions['year'], y=un_sessions['n_members']),
+vote_margin_n_members.add_trace(
+    go.Scatter(x=vote_margin_n_members_df['year'], y=vote_margin_n_members_df['n_members'], name="N Members"),
+    secondary_y=False,
+)
+vote_margin_n_members.add_trace(
+    go.Scatter(x=vote_margin_n_members_df['year'], y=vote_margin_n_members_df['av_vote_margin'], name="Av. Vote Margin"),
     secondary_y=False,
 )
 
-st.plotly_chart(members_year_joined, use_container_width=True)
+st.plotly_chart(vote_margin_n_members, use_container_width=True)
+st.caption("Average vote margin and number of members over time for all resolutions")
 
+# vote margin by number of members for passed resolutions
+vote_margin_n_members_df = pysqldf(q="select avg(vote_margin) as av_vote_margin, avg(n_members) as n_members, year from resolutions where resolution_passed = 1 group by year")
+vote_margin_n_members = make_subplots(specs=[[{"secondary_y": False}]])
+
+# Add traces
+vote_margin_n_members.add_trace(
+    go.Scatter(x=vote_margin_n_members_df['year'], y=vote_margin_n_members_df['n_members'], name="N Members"),
+    secondary_y=False,
+)
+vote_margin_n_members.add_trace(
+    go.Scatter(x=vote_margin_n_members_df['year'], y=vote_margin_n_members_df['av_vote_margin'], name="Av. Vote Margin"),
+    secondary_y=False,
+)
+
+st.plotly_chart(vote_margin_n_members, use_container_width=True)
+st.caption("Average vote margin and number of members over time for passed resolutions")
+
+# vote margin by number of members for not passed resolutions
+vote_margin_n_members_df = pysqldf(q="select avg(vote_margin) as av_vote_margin, avg(n_members) as n_members, year from resolutions where resolution_passed = 0 group by year")
+vote_margin_n_members = make_subplots(specs=[[{"secondary_y": False}]])
+
+# Add traces
+vote_margin_n_members.add_trace(
+    go.Scatter(x=vote_margin_n_members_df['year'], y=vote_margin_n_members_df['n_members'], name="N Members"),
+    secondary_y=False,
+)
+vote_margin_n_members.add_trace(
+    go.Scatter(x=vote_margin_n_members_df['year'], y=vote_margin_n_members_df['av_vote_margin'], name="Av. Vote Margin"),
+    secondary_y=False,
+)
+
+st.plotly_chart(vote_margin_n_members, use_container_width=True)
+st.caption("Average vote margin and number of members over time for resolutions not passed")
+
+st.text('The above 3 graphs gives insight into the relationship between the vote margin and number of voting members.')
+st.text('For passed resolutions, there may be a positive correlation between number of members and vote margin,')
+st.text('this could be a result of the UN becoming more cohesive with increasing number of members,')
+st.text('because an increasing number of members may represent more cohesion among countries and therefore humanitarian values,')
+st.text('represented by decisiveness in voting on resolutions.')
+st.text("This also indicates that when the committee agrees on a matter, they strongly agree.")
+st.text("\n")
+st.text('For resolutions not passed, there seems to be no correlation between number of members and vote margin.')
+st.text("This may indicate that resolutions which split the committee have remained the same over time,")
+st.text("perhaps indicating a world view of those matters which has held over time and among countries.")
+st.text("Interestingly, the last graph may indicate that there is little bias in new member selection")
+st.text("because to maintain the same vote margin over time, new members had to be added in equal proportions")
+st.text("in terms of whether they support contested resolutions or not.")
+
+
+st.text("\n")
+st.subheader("Exploring the Conflicts")
+
+st.text("\n")
+st.text("We now analyse the conflicts, looking at how they have changed over time and what we can understand about this.")
+st.text("To assist the analysis we calculate a metric, intensity, which is the number of casualties per year.")
 
 # conflict casualties vs duration
 casualties_duration_df = pysqldf(q="select avg(casualties) as av_casualties, duration from conflicts where casualties < 14000000 group by duration")
-casualties_duration = make_subplots(specs=[[{"secondary_y": False}]])
 
-# Add traces
-casualties_duration.add_trace(
-    go.Bar(x=casualties_duration_df['duration'], y=casualties_duration_df['av_casualties']),
-    secondary_y=False,
+casualties_duration = px.bar(
+    casualties_duration_df,
+    x='duration',
+    y='av_casualties'
 )
 
 st.plotly_chart(casualties_duration, use_container_width=True)
+st.caption("Conflict Avg. Casualties by Duration")
 
 # conflict intensity vs duration
 intensity_duration_df = pysqldf(q="select avg(intensity) as av_intensity, duration from conflicts where casualties < 14000000 group by duration")
-intensity_duration = make_subplots(specs=[[{"secondary_y": False}]])
 
-# Add traces
-intensity_duration.add_trace(
-    go.Bar(x=intensity_duration_df['duration'], y=intensity_duration_df['av_intensity']),
-    secondary_y=False,
-)
+intensity_duration = px.bar(intensity_duration_df, x='duration', y='av_intensity')
 
 st.plotly_chart(intensity_duration, use_container_width=True)
+st.caption("Conflict Avg. Intensity by Duration")
 
 # conflict start year vs intensity
 intensity_start_df = pysqldf(q="select avg(intensity) as av_intensity, start from conflicts where casualties < 14000000 group by start")
@@ -172,6 +254,7 @@ intensity_start.add_trace(
 )
 
 st.plotly_chart(intensity_start, use_container_width=True)
+st.caption("Conflict Intensity by Start Year")
 
 # conflict start year vs casualties
 casualties_start_df = pysqldf(q="select avg(casualties) as av_casualties, start from conflicts where casualties < 14000000 group by start")
